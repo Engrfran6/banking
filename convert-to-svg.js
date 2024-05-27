4be8fc8986abe436142dbc0f16f1fd2470bec548
Francis <99579671+Engrfran6@users.noreply.github.com>
2024-05-27 19:17:03 +0100

    modified next.config to supress typescript error durring build

--- convert-to-svg.js
+++
@@ -1,8 +0,0 @@
-const potrace = require('potrace');
-const fs = require('fs');
-
-potrace.trace('input.png', (err, svg) => {
-  if (err) throw err;
-  fs.writeFileSync('output.svg', svg);
-  console.log('SVG file saved as output.svg');
-});
